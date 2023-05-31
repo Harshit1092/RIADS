e91ec0cd0c3d988354797b5eb6c52bb010978135
Harshit Gupta <2021csb1092@iitrpr.ac.in>
2023-06-01 03:56:47 +0530

    navigation bar added

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+import {Header} from './components/Header.js';
+
+
+function App() {
+  return (
+  <>
+    <Header />
+  </>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import {useState } from "react";
+
+
+export const Header = () => {
+    const [show, setShow] = useState(false);
+const showDropdown = (e)=>{
+    setShow(!show);
+}
+const hideDropdown = e => {
+    setShow(false);
+}
+  return (
+    <>
+    <Navbar bg="light" expand="lg">
+      <Container fluid>
+        <Navbar.Brand href="#"><img src="./favicon.ico" width="70rem" alt="RIADS" /></Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav className="me-auto my-2 my-lg-0 w-100 justify-content-center" style={{ maxHeight: "auto" }} navbarScroll >
+            <Nav.Link href="#">Home</Nav.Link>
+            <Nav.Link href="#">About Us</Nav.Link>
+            <NavDropdown title="Our Courses" id="navbarScrollingDropdown" show={show} onMouseEnter={showDropdown}  onMouseLeave={hideDropdown}>
+              <NavDropdown.Item href="#" onMouseLeave={hideDropdown}>Refresher Course on HMV Drivers</NavDropdown.Item>
+            </NavDropdown>
+            <Nav.Link href="#">Gallery</Nav.Link>
+            <Nav.Link href="#">Search Registration</Nav.Link>
+            <Nav.Link href="#">Contact Us</Nav.Link>
+             <button type="button" className="bg-[#c54545] px-3 text-white">Pay Now</button>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+    </>
+  )
+}
